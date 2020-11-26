0afe35f23eec4de7464d281f624f2c15e4d94c5b
mddarr <mddarr@gmail.com>
2020-11-26 00:11:41 -0800

    Add another lambda for querying a vendor by a category.  This is a little bit awkward because apparantly FilterExpressions are only available for scans.. so query by partition key, then manually filter.

--- products_orders_application/products-api/query_category_vendor/app.py
+++ products_orders_application/products-api/query_category_vendor/app.py
@@ -1,32 +1,31 @@
-"""
-The lambda function defined here queries the category GSI
-
-"""
-
 import json
 import boto3
 from boto3.dynamodb.conditions import Key
 
 
-dynamo_resource = boto3.resource('dynamodb')  # , endpoint_url='http://dynamo-local:8000')
+# dynamo_resource = boto3.resource('dynamodb')  # , endpoint_url='http://dynamo-local:8000')
+dynamo_resource = boto3.resource('dynamodb')# endpoint_url='http://dynamo-local:8000')
 TABLE_NAME = 'Products'
 table = dynamo_resource.Table(TABLE_NAME)
 
 
-def query_products_by_category(category):
-    response = table.query(
-        # Add the name of the index you want to use in your query.
-        IndexName="categoryGSI",
-        KeyConditionExpression=Key('category').eq(category),
+def query_products_by_vendor_and_category(vendor, category):
+    products = table.query(
+        KeyConditionExpression=Key('vendor').eq(vendor),
     )
-    products = response['Items']
+    products = products['Items']
+    products = [product for product in products if product['category'] == category]
     for product in products:
         product['price'] = float(product['price'])
     return products
 
 
+products = query_products_by_vendor_and_category('Oboz','womens-boots')
+
+
 def lambda_handler(event, context):
     """
+
     Parameters
     ----------
     event: dict, required
@@ -36,22 +35,24 @@ def lambda_handler(event, context):
 
     context: object, required
         Lambda Context runtime methods and attributes
+
         Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
     Returns
     ------
     API Gateway Lambda Proxy Output Format: dict
 
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
-
+    # vendor = event['pathParams']['vendor']
+    vendor = event['pathParameters']['vendor']
     category = event['pathParameters']['category']
-    products = query_products_by_category(category)
+    products = query_products_by_vendor_and_category(vendor, category)
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "products": products,
-            # "type": str(event.keys())
-            # "location": ip.text.replace("\n", "")
+            "products": str(products),
+
         }),
     }
